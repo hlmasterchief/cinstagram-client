1cb213564231f670e93a93fdc8545883eb1e5278
HL <hlmasterchief93@gmail.com>
2015-10-04 20:47:05 +0300

    add basic route/page

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,114 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/app.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'app-home': {
+        templateUrl: 'templates/app-home.html'
+      }
+    }
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'app-search': {
+        templateUrl: 'templates/app-search.html'
+      }
+    }
+  })
+
+  .state('app.camera', {
+    url: '/camera',
+    views: {
+      'app-camera': {
+        templateUrl: 'templates/app-camera.html'
+      }
+    }
+  })
+
+  .state('app.activity', {
+    url: '/activity',
+    views: {
+      'app-activity': {
+        templateUrl: 'templates/app-activity.html'
+      }
+    }
+  })
+
+  .state('app.activity.following', {
+    url: '/following',
+    views: {
+      'activity-following': {
+        templateUrl: 'templates/activity-following.html'
+      }
+    }
+  })
+
+  .state('app.activity.you', {
+    url: '/you',
+    views: {
+      'activity-you': {
+        templateUrl: 'templates/activity-you.html'
+      }
+    }
+  })
+
+  .state('app.profile', {
+    url: '/profile',
+    views: {
+      'app-profile': {
+        templateUrl: 'templates/app-profile.html'
+      }
+    }
+  })
+
+  .state('welcome', {
+    url: '/welcome',
+    absract: true,
+    templateUrl: 'templates/welcome.html'
+  })
+
+  .state('welcome.signin', {
+    url: '/signin',
+    views: {
+      'welcome-signin': {
+        templateUrl: 'templates/welcome-signin.html'
+      }
+    }
+  })
+
+  .state('welcome.signup', {
+    url: '/signup',
+    views: {
+      'welcome-signup': {
+        templateUrl: 'templates/welcome-signup.html'
+      }
+    }
+  })
+
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('welcome/signin');
+
+});
